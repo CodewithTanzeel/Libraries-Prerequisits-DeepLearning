bc321e34b0cc8ec614fbb39cc4322f86c4644bde
CodewithTanzeel <Tanzeelofficial@outlook.com>
2025-09-02 12:11:07 +0500

    videos

--- open-cv/opencv-01/read.py
+++ open-cv/opencv-01/read.py
@@ -1,7 +1,20 @@
 import cv2 as cv
+## Reading Images
+# img = cv.imread('Photos/cat01.jpg')
+# cv.imshow('Cat',img)
 
-img = cv.imread('Photos/cat01.jpg')
-cv.imshow('Cat',img)
+##Reading Videos
+capture = cv.VideoCapture('Videos/CatVideo-01.mp4')
+while True:
+    isTrue,frame= capture.read()
+    cv.imshow('Video',frame)
+    
+    if cv.waitKey(20) & 0xFF==ord('b'):
+        break
+    
+capture.release()
+cv.destroyAllWindows()
+    
 
-cv.waitKey(0)
+# cv.waitKey(0)
 
