2fbf5b133900b32b2a6caec4744d4443021afd83
CodewithTanzeel <Tanzeelofficial@outlook.com>
2025-09-02 13:11:25 +0500

    .

--- open-cv/opencv-01/draw.py
+++ open-cv/opencv-01/draw.py
@@ -3,16 +3,18 @@ import numpy as np
 blank = np.zeros((500,500,3),dtype='uint8')
 cv.imshow('Blank',blank)
 #paint the image a Certain color,
-blank[:] = 0,255,213
-cv.imshow('Musturd',blank)
+# blank[:] = 0,255,213
+# cv.imshow('Musturd',blank)
 # Color in range of pixels
-blank[200:300,300:400] = 0,255,0
-cv.imshow('Musturd',blank)
+# blank[200:300,300:400] = 0,255,0
+# cv.imshow('Musturd',blank)
 #Drawing a rectangle
-cv.rectangle(blank,(0,0),(250,250),(0,250,0),thickness=cv.FILLED)
-cv.imshow("rectangle",blank)
+# cv.rectangle(blank,(0,0),(250,250),(0,250,0),thickness=cv.FILLED)
+# cv.imshow("rectangle",blank)
 
 
+cv.putText(blank,'This',(255,255),cv.FONT_HERSHEY_SCRIPT_COMPLEX,1.0,(0,0,255),2)
+cv.imshow("Text",blank)
 img = cv.imread('Photos/cat01.jpg')
 cv.imshow("Cat",img)
 
