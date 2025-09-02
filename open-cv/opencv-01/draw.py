552c43f284153402a14d8c5ae8de7b10a7f1f8f0
CodewithTanzeel <Tanzeelofficial@outlook.com>
2025-09-02 13:02:01 +0500

    Update draw.py

--- open-cv/opencv-01/draw.py
+++ open-cv/opencv-01/draw.py
@@ -5,9 +5,16 @@ cv.imshow('Blank',blank)
 #paint the image a Certain color,
 blank[:] = 0,255,213
 cv.imshow('Musturd',blank)
+# Color in range of pixels
+blank[200:300,300:400] = 0,255,0
+cv.imshow('Musturd',blank)
+#Drawing a rectangle
+cv.rectangle(blank,(0,0),(250,250),(0,250,0),thickness=cv.FILLED)
+cv.imshow("rectangle",blank)
+
 
 img = cv.imread('Photos/cat01.jpg')
 cv.imshow("Cat",img)
 
 
-cv.waitKey(0)
+cv.waitKey(0) 
