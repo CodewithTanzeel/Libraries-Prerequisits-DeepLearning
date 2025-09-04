9bbc155145ead740680be49fa31b82429fd4a640
CodewithTanzeel <Tanzeelofficial@outlook.com>
2025-09-04 15:10:30 +0500

    >

--- open-cv/opencv-facedec/face_detect.py
+++ open-cv/opencv-facedec/face_detect.py
@@ -7,3 +7,19 @@ cv.imshow("image",img)
 
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow("GrayScale image",gray)
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+faces_rect = haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3)
+
+print(f'Number of faces found ={len(faces_rect)}')
+
+
+
+for(x,y,w,h) in faces_rect:
+    cv.rectangle(img,(x,y),(x+w,y+h),(0,225,0),thickness = 2)
+
+cv.imshow("Dectected Faces",img)
+
+
+
+
+cv.waitKey(0)
